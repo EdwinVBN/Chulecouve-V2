37c47e937aaec896d921b67e219662f12f46f877
JespervnVeen <ninjahamstr@gmail.com>
2024-04-24 20:40:59 +0200

    SCSS

--- HoBo/resources/views/home.blade.php
+++ HoBo/resources/views/home.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('SCSS/styles.css') }}">
 </head>
 <body>
-    <h1>Yo</h1>
+    <h1 id='test'>Yo</h1>
 </body>
 </html>
