90cf8c58bba6db1beb878f2b06d390f0aaefe02b
JespervnVeen <ninjahamstr@gmail.com>
2024-04-26 15:39:38 +0200

    home

---
+++ HoBo/resources/views/header.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('SCSS/styles.css') }}">
+</head>
+<body>
+    <header>
+        <article id="header-left">
+            <a href="">Home</a>
+            <a href="">Genre</a>
+        </article>
+        <article id="header-logo">
+            <img src="img/HOBO_Beeldmerk.png">
+        </article>
+        <article id="header-right">
+            <a href="">Profiel</a>
+            <a href="">Zoek</a>
+        </article>    
+    </header>
+</body>
+</html>
--- HoBo/resources/views/home.blade.php
+++ HoBo/resources/views/home.blade.php
@@ -6,7 +6,30 @@
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('SCSS/styles.css') }}">
 </head>
-<body>
-    <h1>Yo</h1>
+<body id="home-body">
+    @include('header')
+    <main>
+        <section id="home-info">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet. Non exercitationem consequatur et repellendus minima qui dolorem laboriosam. Qui rerum maxime cum odit corporis ea minus asperiores ad impedit impedit. Vel dicta rerum et possimus dolorum est ducimus rerum. Nam consequuntur pariatur in totam tempore sit nulla sint qui enim atque in consectetur nemo et velit sint 33 aliquid voluptatem.</p>
+            <button id="watch">Kijk</button>
+        </section>
+        <section id="carousel-trending">
+            <h1>Trending</h1>
+            <section class="carousel-images">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+                <img src="img/test.jpg">
+            </section>
+        </section>
+    </main>
 </body>
 </html>
