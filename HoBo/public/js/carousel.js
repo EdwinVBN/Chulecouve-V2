f240f97ba48412ba8a8efcaee9a566405db7b186
JespervnVeen <ninjahamstr@gmail.com>
2024-04-28 20:27:27 +0200

    verbeter

--- HoBo/public/js/carousel.js
+++ HoBo/public/js/carousel.js
@@ -2,33 +2,34 @@ class Slider {
     constructor() {
         this.slider = document.querySelector('.carousel-images');
         this.images = document.querySelectorAll('.carousel-image');
-        this.containerWidth = this.slider.clientWidth;
         this.imageWidth = this.images[0].clientWidth;
-        this.onScreen = Math.floor(this.containerWidth / this.imageWidth);
         this.index = 0;
     }
 
     next() {
-        if (this.index < this.images.length - this.onScreen) {
+        if (this.index < this.images.length) {
             this.index++;
             this.slide();
+            this.images[this.index - 1].style.transition = "opacity 0.8s ease";
+            this.images[this.index - 1].style.opacity = 0.5;  
         }
     }
 
     prev() {
+        let adjust = this.index - 1;
+    
         if (this.index > 0) {
             this.index--;
             this.slide();
+            this.images[adjust].style.transition = "opacity 0.8s ease";
+            this.images[adjust].style.opacity = 1;
         }
     }
 
     slide() {
-        const offset = -this.index * this.containerWidth;
+        const offset = -this.index * (this.imageWidth + 30);
+        this.slider.style.transition = 'transform 0.3s ease-in-out';
         this.slider.style.transform = `translateX(${offset}px)`;
-    }
-
-    updateWidth() {
-        this.containerWidth = this.slider.clientWidth;
     }
 }
 
@@ -36,4 +37,3 @@ const slider = new Slider();
 
 document.getElementById("next").addEventListener('click', () => slider.next());
 document.getElementById("prev").addEventListener('click', () => slider.prev());
-window.addEventListener('resize', () => slider.updateWidth());
