72a963d5afc6f7d30358b81ff9903dd64d80cb35
JespervnVeen <ninjahamstr@gmail.com>
2024-05-01 12:48:21 +0200

    videos

---
+++ HoBo/resources/views/filminfo.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('SCSS/styles.css') }}">
+</head>
+<body id="home-body">
+    @include('header')
+    <main>
+        <section id="home-info">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet. Non exercitationem consequatur et repellendus minima qui dolorem laboriosam. Qui rerum maxime cum odit corporis ea minus asperiores ad impedit impedit. Vel dicta rerum et possimus dolorum est ducimus rerum. Nam consequuntur pariatur in totam tempore sit nulla sint qui enim atque in consectetur nemo et velit sint 33 aliquid voluptatem.</p>
+            <button id="watch">Kijk</button>
+        </section>
+</body>
+</html>
