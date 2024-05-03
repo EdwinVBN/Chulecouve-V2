d80d641dc0b039d1d2f4480479b0d9c5020bf5ef
JespervnVeen <ninjahamstr@gmail.com>
2024-05-03 22:28:10 +0200

    filminfo page

--- HoBo/resources/views/filminfo.blade.php
+++ HoBo/resources/views/filminfo.blade.php
@@ -14,5 +14,11 @@
             <p>Lorem ipsum dolor sit amet. Non exercitationem consequatur et repellendus minima qui dolorem laboriosam. Qui rerum maxime cum odit corporis ea minus asperiores ad impedit impedit. Vel dicta rerum et possimus dolorum est ducimus rerum. Nam consequuntur pariatur in totam tempore sit nulla sint qui enim atque in consectetur nemo et velit sint 33 aliquid voluptatem.</p>
             <button id="watch">Kijk</button>
         </section>
+        <section id="info-movie">
+            <h1>Genre: </h1>
+            <h1>Regisseur: </h1>
+            <h1>Rating: </h1>
+        </section>
+    </main>
 </body>
 </html>
